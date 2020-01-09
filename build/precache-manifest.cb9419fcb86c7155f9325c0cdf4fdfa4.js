0160cd9b537458c5c1ed89a4bebaf066d05fefbe
raraku <airenov500@gmail.com>
2020-01-09 20:48:27 +0100

    work now

---
+++ build/precache-manifest.cb9419fcb86c7155f9325c0cdf4fdfa4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7504b2a04715ae298b5e75112ff092d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f3a9f9083cfe47a8637",
+    "url": "/static/css/2.6ebcfcee.chunk.css"
+  },
+  {
+    "revision": "82b7dad08bdce3c0c6da",
+    "url": "/static/css/main.2d5527e5.chunk.css"
+  },
+  {
+    "revision": "0f3a9f9083cfe47a8637",
+    "url": "/static/js/2.c0862eeb.chunk.js"
+  },
+  {
+    "revision": "82b7dad08bdce3c0c6da",
+    "url": "/static/js/main.21265400.chunk.js"
+  },
+  {
+    "revision": "60f05623933eb017ae52",
+    "url": "/static/js/runtime-main.78c084b0.js"
+  },
+  {
+    "revision": "a45f1a86bf9beb87c527ce788836320a",
+    "url": "/static/media/Bean.a45f1a86.svg"
+  },
+  {
+    "revision": "e41cf3020a9878f265929d0454716973",
+    "url": "/static/media/background.e41cf302.svg"
+  }
+]);
